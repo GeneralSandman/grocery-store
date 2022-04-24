05fed6f42bda2ba036fdcc77ad589db535f7ae4c
zhenhuli <zhenhuli@tencent.com>
2022-04-24 11:15:56 +0800

    add: solution

--- grocery-algorithm/剑指Offer/个人题解-源码/020.cpp
+++ grocery-algorithm/剑指Offer/个人题解-源码/020.cpp
@@ -9,7 +9,24 @@ using namespace std;
 class Solution {
 public:
     int countSubstrings(string s) {
+        int result = 0;
 
+        for (int i = 0; i < s.size();i++) {
+            result += helper(s, i, i);
+            result += helper(s, i, i + 1);
+        }
+        return result;
+    }
+
+    int helper(string &s, int begin, int end) {
+        int result = 0;
+        while (0 <= begin && end < s.size() && s[begin] == s[end]) {
+            begin--;
+            end++;
+            result++;
+        }
+
+        return result;
     }
 };
 
